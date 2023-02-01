acb9d108483fd7801e82ffe566136ca4a83d6108
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2023-02-01 08:53:31 -0800

    added ukrainan flag

--- src/components/firstScreen/firstScreen.js
+++ src/components/firstScreen/firstScreen.js
@@ -8,7 +8,7 @@ export default function FirstScreen(){
         <div className="fisrtsection flex w-full pt-[100px] xs:pt-[100px] pb-[55px] xs:flex-col xs:gap-[50px] box-border overflow-hidden xl:justify-center">
             <div className="relative w-[600px] xs:w-full pt-[70px] pl-[140px] xs:pl-[40px] pr-[60px] flex xs:h-[310px]">
                 <img  src={flights} alt='flights' className='absolute w-[329px] xs:w-[250px] top-0'/>
-                {/* <img  src={ukr} alt='flights' className='absolute w-[50px] xs:w-[50px] top-[50px]'/> */}
+                <img  src={ukr} alt='flights' className='absolute w-[50px] xs:w-[50px] top-[30px] left-[370px] xs:top-[10px] xs:left-[200px]'/>
 
                 <svg className="absolute rotate-45 w-[329px] h-[420px] xs:w-[250px] xs:h-[310px] xs:translate-x-[-50%] xs:left-[50%]" viewBox="0 0 509 600" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M422.532 68.196C391.035 24.3898 349.711 -2.42238 296.681 0.806346C222.818 5.30335 160.936 29.8867 112.066 73.197C61.5193 117.994 24.8943 182.826 3.33521 266.189C-16.0326 341.078 68.2447 422.431 171.312 485.57C293.894 560.663 448.648 614.358 481.634 596.406C494.444 589.434 498.712 554.096 504.077 509.677L504.088 509.58C518.4 391.089 500.843 177.114 422.532 68.196Z" fill="#E0F9FC" fillOpacity="1"/>
